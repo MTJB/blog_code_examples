49938d1a4ee3615e7592ea91a9fd6391011db784
Mark Brown <markbrown04@googlemail.com>
2020-10-18 17:07:32 +0100

    Initial commit - add spring project from spring initializr

---
+++ src/test/java/com/mtjb/examples/ExamplesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mtjb.examples;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamplesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
